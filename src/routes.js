c8004737fb2ddb00998a233e6f7a756a8c859fad
devrahul95 <rbhadoria97@gmail.com>
2022-06-15 15:06:47 +0530

    project added

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+import Dashboard from "layouts/dashboard";
+import Tables from "layouts/tables";
+import Notifications from "layouts/notifications";
+import SignIn from "layouts/authentication/sign-in";
+
+// @mui icons
+import Icon from "@mui/material/Icon";
+
+const routes = [
+  {
+    type: "collapse",
+    name: "Dashboard",
+    key: "dashboard",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/dashboard",
+    component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Tables",
+    key: "tables",
+    icon: <Icon fontSize="small">table_view</Icon>,
+    route: "/tables",
+    component: <Tables />,
+  },
+  {
+    type: "collapse",
+    name: "Notifications",
+    key: "notifications",
+    icon: <Icon fontSize="small">notifications</Icon>,
+    route: "/notifications",
+    component: <Notifications />,
+  },
+  {
+    type: "collapse",
+    name: "Sign In",
+    key: "sign-in",
+    icon: <Icon fontSize="small">login</Icon>,
+    route: "/authentication/sign-in",
+    component: <SignIn />,
+  },
+];
+
+export default routes;
