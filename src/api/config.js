4f3592de0c342380d05fa8e5d64403cec661da26
Vishal Chouhan <support24@anaxee.com>
2022-08-27 17:24:43 +0530

    change in config file

--- src/api/config.js
+++ src/api/config.js
@@ -5,8 +5,8 @@ import cookies from "js-cookie";
 //export const mainUrl = `https://still-depths-36066.herokuapp.com`;
 
 // local testing
-export const mainUrl = `http://localhost:8001`;
-//  export const mainUrl = `https://android-mapping-backend.el.r.appspot.com`;
+// export const mainUrl = `http://localhost:8001`;
+ export const mainUrl = `https://android-mapping-backend.el.r.appspot.com`;
 const baseDomain = `${mainUrl}/`;
 
 
