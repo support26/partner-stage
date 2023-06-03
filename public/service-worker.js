705f257dc253e6ac677afcec88f862fcd8b20e9d
vishal <support24@anaxee.com>
2023-06-03 11:38:59 +0530

    add push notification

---
+++ public/service-worker.js
@@ -0,0 +1,56 @@
+self.addEventListener('push', function(e) {
+    const data = e.data.json();
+    console.log("Push received...", data);
+    self.registration.showNotification(
+        data.title,
+        {
+            body: data.body,
+        }
+    );
+})
+
+
+// const CACHE_NAME = 'pwa-cache-v1';
+// const urlsToCache = [
+//     '/',
+//     '/index.html',
+//     '/css/style.css',
+//     '/js/app.js',
+//     '/js/bundle.js',
+//     '/js/script.js',
+//     '/images/AnaxeeLogo.ico',
+//     '/images/icons/favicon.png',
+// ]
+
+// self.addEventListener('install', function(e) {
+//     e.waitUntil(
+//         caches.open(CACHE_NAME)
+//         .then(function(cache) {
+//             console.log("Opened cache");
+//             return cache.addAll(urlsToCache);
+//         })
+//     ); 
+// });
+
+// self.addEventListener('fetch', function(e) {
+//     e.respondWith(
+//         caches.match(e.request)
+//         .then(function(response) {
+//             if(response) {
+//                 return response;
+//             }
+//         })
+//     );
+// });
+
+self.addEventListener('notificationclick', function(e) {
+    const notification = e.notification;
+    const action = e.action;
+    if(action === 'close') {
+        notification.close();
+    }
+    else {
+        clients.openWindow('https://partner.anaxee.com/ticket');
+        notification.close();
+    }
+})
