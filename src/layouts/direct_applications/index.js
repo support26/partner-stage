82d0c534f875ee1b8c841fa1e2003a554eacec4a
support9-anaxee <1peix5pucdfqtaymxlnpmxhcf7ljo4@bots.bitbucket.org>
2023-09-05 17:51:02 +0530

    fix dialog

--- src/layouts/direct_applications/index.js
+++ src/layouts/direct_applications/index.js
@@ -3,10 +3,7 @@ import { useState, useEffect } from "react";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-
 import {
   DataGrid,
   GridToolbarContainer,
